0c5b7e13fbf49a0a439f1876e7c97fc62ec7d0b5
Ralph Avalon <raphael_amoedo@hotmail.com>
2015-04-25 14:59:30 -0300

    Adding timestamp on easy_date.py

--- easy_date.py
+++ easy_date.py
@@ -10,6 +10,8 @@ def convert_from_string(string, current_format, to_format, to_type=str):
         return converter.string_to_datetime(string, current_format, to_format)
     elif to_type == date:
         return converter.string_to_date(string, current_format)
+    elif to_type == float:
+        return converter.string_to_timestamp(string, current_format)
 
 def convert_from_datetime(from_datetime, to_format, to_type=str):
     if to_type == str:
@@ -18,9 +20,21 @@ def convert_from_datetime(from_datetime, to_format, to_type=str):
         return converter.datetime_to_datetime(from_datetime, to_format)
     elif to_type == date:
         return converter.datetime_to_date(from_datetime)
+    elif to_type == float:
+        return converter.datetime_to_timestamp(from_datetime)
 
 def convert_from_date(from_date, to_format, to_type=str):
     if to_type == str:
         return converter.date_to_string(from_date, to_format)
     elif to_type == datetime:
         return converter.date_to_datetime(from_date)
+    elif to_type == float:
+        return converter.date_to_timestamp(from_date)
+
+def convert_from_timestamp(timestamp, to_format, to_type=str):
+    if to_type == str:
+        return converter.timestamp_to_string(timestamp, to_format)
+    elif to_type == datetime:
+        return converter.timestamp_to_datetime(timestamp, to_format)
+    elif to_type == date:
+        return converter.timestamp_to_date(timestamp)
