f80c3abf5c7f22a5f98305ffb2ad6c8e50bc216d
Raphael Amoedo <avalon.ralph@gmail.com>
2017-07-22 13:28:26 -0300

    Adjusting travis_setup.sh to set time

--- travis_setup.sh
+++ travis_setup.sh
@@ -1,4 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y ntpdate
 sudo ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+sudo service ntp stop
 sudo ntpdate a.ntp.br
+sudo service ntp start
